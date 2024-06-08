19df5aba0f9fd0334c719881155470004394dfea
Rakshit78 <rakshitsootwal@yahoo.com>
2024-06-08 14:26:18 +0530

    presist data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import productReducer from './slice/products';
-// export const store = configureStore({
-//   reducer: {
-//     product: productReducer,
-//   },
-// });
-
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './slice/products.js';
 export const store = configureStore({
